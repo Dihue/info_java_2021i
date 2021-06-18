f018c6b430de407ceeb8ba9d473dc2ae9098a458
Dihue <dihue21@gmail.com>
2021-06-17 21:57:35 -0300

    Complementario Nivel 1-1 completado

--- src/com/informatorio/complementaryLevel1/Exercise_1.java
+++ src/com/informatorio/complementaryLevel1/Exercise_1.java
@@ -1,2 +1,24 @@
-package com.informatorio.complementaryLevel1;public class Exercise_1 {
+package com.informatorio.complementaryLevel1;
+
+import java.util.Scanner;
+/*
+Solicitar por consola el nombre del usuario e imprimir por pantalla el
+siguente mensaje "HOLA {USUARIO}
+ */
+public class Exercise_1 {
+    public static void main(String[] args) {
+        // Objeto scanner para tomar ingreso por consola
+        Scanner input = new Scanner(System.in);
+
+        // Aviso por pantalla
+        System.out.print("Ingresar nombre de usuario: ");
+        // Escanea la siguente palabra o cadena de palabras
+        String name = input.nextLine();
+
+        // Finalizar el proceso de escaneo
+        input.close();
+
+        // Salida por pantalla
+        System.out.println("HOLA " + name.toUpperCase());
+    }
 }
